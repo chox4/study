38ef64eb107b19abcd44c7d272799c951bde5869
rbghks725 <rbghks725@naver.com>
2020-02-27 20:43:33 +0900

    commit

---
+++ No.10951/No.10951/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  No.10951
+//
+//  Created by kyuhwan cho on 2020/02/27.
+//  Copyright © 2020 kyuhwan cho. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int a,b;
+    while(cin>>a>>b){        
+        cout<<a+b<<"\n";
+    }
+    return 0;
+}
